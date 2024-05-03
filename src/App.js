3c86f8fba6fb71203871e6fa12037297d9559058
Noor-Abdelmuneim <157888524+Noor-Abdelmuneim@users.noreply.github.com>
2024-05-03 13:28:54 +0300

    done with css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Buttons from './Buttons';
+import Header from './Header';
+import Posts from './Posts';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div style={{ display: "flex", width: "60%" }}>
+          <div style={{ width: "70%" }}>
+            <Posts />
+            <Posts />
+            <Posts />
+            <Posts />
+            <Posts />
+          </div>
+          <div style={{ width: "30%" }}>
+            <Buttons />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
 
 export default App;
